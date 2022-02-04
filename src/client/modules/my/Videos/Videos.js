99b1e111a7e590ee3c3513bb96f78abe766c55ee
PatrickSepnio <pat2thoams@gmail.com>
2022-02-04 13:04:30 -0600

    Added Header
    Modified Header component

--- src/client/modules/my/Videos/Videos.js
+++ src/client/modules/my/Videos/Videos.js
@@ -1,3 +1,20 @@
 import { LightningElement } from 'lwc';
 
-export default class Videos extends LightningElement {}
+export default class Videos extends LightningElement {
+    renderForYou= true;
+    renderNews= true;
+    renderSports= true;
+
+    switchForYou() {
+        this.renderForYou=false;
+    }
+
+    switchNews() {
+        this.renderNews=false;
+    }
+
+    switchSports() {
+        this.renderSports=false;
+    }
+
+}
